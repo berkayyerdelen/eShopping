28453e0bef7f6ecefe9f0a489bb8a6233799ebf6
Berkay Yerdelen <berkayyerdelen@gmail.com>
2020-07-26 17:54:24 +0300

    imp discount test

--- test/eShopping.DomainTests/DiscountTest.cs
+++ test/eShopping.DomainTests/DiscountTest.cs
@@ -1,9 +1,41 @@
-﻿using Xunit;
+﻿using eShopping.Domain.ShoppingCart.Enums;
+using eShopping.Domain.ShoppingCart.ValueObject;
+using Moq;
+using Xunit;
 
 namespace eShopping.DomainTests
 {
     public class DiscountTest
     {
-       
+        public Mock<Discount> MockDiscount { get; set; }
+        public DiscountTest()
+        {
+            MockDiscount = new Mock<Discount>();
+        }
+
+        [Fact]
+        public void Discount_ShouldCalculate_When_DiscountType_Amount()
+        {
+            MockDiscount.Setup(x => x.Calculate(DiscountType.Amount, 100, 20)).Returns(20);
+            var calculatedDiscount =MockDiscount.Object.Calculate(DiscountType.Amount, 100, 20);
+            Assert.Equal(20, calculatedDiscount);
+        }
+
+        [Fact]
+        public void Discount_ShouldCalculate_When_DiscountType_Rate()
+        {
+            MockDiscount.Setup(x => x.Calculate(DiscountType.Rate, 100, 20)).Returns(40);
+            var calculatedDiscount = MockDiscount.Object.Calculate(DiscountType.Rate, 100, 20);
+            Assert.Equal(40, calculatedDiscount);
+        }
+
+        [Fact]
+        public void Discount_ShouldReturn_CalculateDiscount_WithRate()
+        {
+            MockDiscount.Setup(x => x.Calculate(DiscountType.Amount, 100, 20)).Returns(20);
+            var calculatedDiscount = MockDiscount.Object.Calculate(DiscountType.Amount, 100, 20);
+        }
+
     }
+    
 }
