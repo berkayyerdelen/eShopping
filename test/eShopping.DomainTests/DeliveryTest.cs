175f9a9d85e290e002ca001d576cfc1b664fdbf8
Berkay Yerdelen <berkayyerdelen@gmail.com>
2020-07-26 19:51:40 +0300

    -imp deliverytest

---
+++ test/eShopping.DomainTests/DeliveryTest.cs
@@ -0,0 +1,27 @@
+﻿using eShopping.Domain.ShoppingCart;
+using eShopping.Domain.ShoppingCart.ValueObject;
+using Xunit;
+
+namespace eShopping.DomainTests
+{
+    public class DeliveryTest
+    {
+        [Fact]
+        public void Delivery_Should_CreateDelivery()
+        {
+            var delivery = new Delivery(5,5,5);
+            Assert.Equal(5,delivery.CostPerDelivery);
+            Assert.Equal(5,delivery.CostPerProduct);
+            Assert.Equal(5,delivery.FixedCost);
+        }
+
+        [Fact]
+        public void Delivery_Should_Calculate()
+        {
+            var delivery = new Delivery(1, 1, 1);
+            var cart = new Cart();
+            cart.AddProduct(new Product("testProduct",1,new Category("testCategory")),1);
+            Assert.Equal(3,delivery.Calculate(cart)); 
+        }
+    }
+}
---
+++ test/eShopping.DomainTests/ProductTest.cs
@@ -0,0 +1,17 @@
+﻿using eShopping.Domain.ShoppingCart.ValueObject;
+using Xunit;
+
+namespace eShopping.DomainTests
+{
+    public class ProductTest
+    {
+        [Fact]
+        public void Product_ShouldCreate_Product()
+        {
+            var product  = new Product("test",20, new Category("testCategory"));
+            Assert.Equal("test",product.Title);
+            Assert.Equal(20,product.Price);
+            Assert.Equal("testCategory",product.Category.Title);
+        }
+    }
+}
